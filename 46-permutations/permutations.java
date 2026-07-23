1933ff1a22d64a01fde22eb003b97cfba37de03a
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-07-24 00:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (90.29%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,11 +1,13 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
 
-        boolean[] visited = new boolean[n];
+        // boolean[] visited = new boolean[n];
         List<List<Integer>> list = new ArrayList<>();
 
-        findPermutation(nums, list, visited, new ArrayList<>());
+        // findPermutation(nums, list, visited, new ArrayList<>());
+
+        permute(nums, 0, list);
 
         return list;
 
@@ -13,23 +15,47 @@ class Solution {
 
     }
 
-    public void findPermutation(int[] nums, List<List<Integer>> res, boolean visited[], List<Integer> list){
-        if(list.size() == nums.length){
-            res.add(new ArrayList<>(list));
-            return;
-        }
+    // public void findPermutation(int[] nums, List<List<Integer>> res, boolean visited[], List<Integer> list){
+    //     if(list.size() == nums.length){
+    //         res.add(new ArrayList<>(list));
+    //         return;
+    //     }
 
-        for(int i = 0; i < nums.length; i++){
-            if(!visited[i]){
-                list.add(nums[i]);
-                visited[i] = true;
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(!visited[i]){
+    //             list.add(nums[i]);
+    //             visited[i] = true;
 
-                findPermutation(nums, res, visited, list);
+    //             findPermutation(nums, res, visited, list);
 
-                list.remove(list.size() - 1);
-                visited[i] = false;
+    //             list.remove(list.size() - 1);
+    //             visited[i] = false;
 
+    //         }
+    //     }
+    // }
+
+    public void permute(int[] nums, int idx, List<List<Integer>> list){
+        if(idx == nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++){
+                temp.add(nums[i]);
             }
+
+            list.add(temp);
+            
         }
+
+        for(int i = idx; i < nums.length; i++){
+                swap(i, idx, nums);
+                permute(nums, idx + 1, list);
+                swap(i, idx, nums);
+            }
+    }
+
+    public void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
