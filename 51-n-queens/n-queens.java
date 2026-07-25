b3f57438af37a9119bfc8899c1a6d17eefb10aab
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-07-25 18:52:08 +0530

    Time: 1 ms (99.86%) | Memory: 46.4 MB (82.42%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -2,67 +2,50 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
 
         List<List<String>> list = new ArrayList<>();
+
         char[][] board = new char[n][n];
 
         for(int i = 0; i < n; i++){
             Arrays.fill(board[i], '.');
         }
-        solve(list, board, n, 0);
+
+        int[] side = new int[n];
+        int[] upper = new int[2 * n - 1];
+        int[] lower = new int[2 * n - 1];
+
+        solve(list, board, n, 0, side, upper, lower);
 
         return list;
+
+        
     }
 
-
-    public void solve(List<List<String>> list, char[][] board, int n, int col){
+    public void solve(List<List<String>> list, char[][] board, int n, int col, int[] side, int[] upper, int[] lower){
         if(col == n){
             List<String> temp = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
+            for(int i = 0; i < n; i++){
                 temp.add(new String(board[i]));
             }
+
             list.add(temp);
             return;
         }
 
         for(int row = 0; row < n; row++){
-            if(isSafe(row , col, n, board)){
-                board[row][col] = 'Q';         
-                
-                solve(list, board, n, col + 1); 
-               
+            if(side[row] == 0 && lower[col + row] == 0 && upper[n - 1 + col - row] == 0){
+                board[row][col] = 'Q';
+                side[row] = 1;
+                lower[col + row] = 1; 
+                upper[n - 1 + col - row] = 1;
+                solve(list, board, n, col + 1, side, upper, lower);
                 board[row][col] = '.';
+                side[row] = 0;
+                lower[col + row] = 0; 
+                upper[n - 1 + col - row] = 0;
+
             }
         }
     }
 
-    public boolean isSafe(int row, int col, int n, char[][] board){
-        int duprow = row;
-        int dupcol = col;
 
-        while(row >= 0 && col >= 0){
-            if(board[row][col] == 'Q') return false;
-
-            row--;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-
-        while(col >= 0){
-            if(board[row][col] == 'Q') return false;
-
-            col--;
-        }
-
-        col = dupcol;
-
-        while(row < n && col >= 0){
-            if(board[row][col] == 'Q') return false;
-
-            row++;
-            col--;
-        }
-
-        return true;
-    }
 }
