e117b3331e1d8aa6fa27a9babaae713aa52ba55c
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-07-31 00:10:40 +0530

    Time: 2 ms (98.17%) | Memory: 47.3 MB (55.53%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -14,26 +14,87 @@
  * }
  */
 class Solution {
+    
     public boolean findTarget(TreeNode root, int k) {
-        List<Integer> list = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        while(true){
-            if(root != null){
+        // List<Integer> list = new ArrayList<>();
+        // Stack<TreeNode> stack = new Stack<>();
+        // while(true){
+        //     if(root != null){
+        //         stack.push(root);
+        //         root = root.left;
+        //     }else{
+        //         if(stack.isEmpty()) break;
+        //         TreeNode top = stack.pop();
+        //         list.add(top.val);
+        //         root = top.right;
+        //     }
+        // } 
+        // int l = 0, r = list.size() - 1;
+        // while(l < r){
+        //     if(list.get(l) + list.get(r) == k) return true;
+        //     else if(list.get(l) + list.get(r) > k) r--;
+        //     else l++;
+        // }
+        // return false;
+
+        BSTIterator l = new BSTIterator(root, false);
+        BSTIterator r = new BSTIterator(root, true);
+
+        int i = l.next();
+        int j = r.next();
+
+        while(i < j){
+            if(i + j == k) return true;
+            else if(i + j > k) j = r.next();
+            else i = l.next();
+        }
+
+        return false;
+    }
+     
+    
+}
+class BSTIterator{
+    Stack<TreeNode> stack = new Stack<>();
+    boolean reverse;
+    public BSTIterator(TreeNode root, boolean reverse){
+        this.reverse = reverse;
+        push(root);
+    }
+
+    public void push(TreeNode root){
+        if(reverse){
+            while(root !=  null){
+                stack.push(root);
+                root = root.right;
+            }
+        }else{
+            while(root != null){
                 stack.push(root);
                 root = root.left;
-            }else{
-                if(stack.isEmpty()) break;
-                TreeNode top = stack.pop();
-                list.add(top.val);
-                root = top.right;
             }
-        } 
-        int l = 0, r = list.size() - 1;
-        while(l < r){
-            if(list.get(l) + list.get(r) == k) return true;
-            else if(list.get(l) + list.get(r) > k) r--;
-            else l++;
         }
-        return false;
+    }
+
+    public int next(){
+        if(reverse){
+            TreeNode top = stack.pop();
+            int ans = top.val;
+            TreeNode curr = top.left;
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.right;
+            }
+            return ans;
+        }else{
+            TreeNode top = stack.pop();
+            int ans = top.val;
+            TreeNode curr = top.right;
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            return ans;
+        }
     }
 }
