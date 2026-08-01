187ef9ddca550e4369f8b811b6d3a04778af0d33
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-08-01 22:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (62.62%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,33 +1,33 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length, n = image[0].length;
-        int[][] mat = new int[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                mat[i][j] = image[i][j];
-            }
+        // int m = image.length, n = image[0].length;
+        // int[][] mat = new int[m][n];
+        // for(int i = 0; i < m; i++){
+        //     for(int j = 0; j < n; j++){
+        //         mat[i][j] = image[i][j];
+        //     }
+        // }
+       
+        int originalColor = image[sr][sc];
+        if (originalColor == color) {
+            return image;
         }
-        boolean[][] visited = new boolean[m][n];
-        int originalColor = mat[sr][sc];
-        dfs(mat, sr, sc, color, originalColor, visited);
-        return mat;
+        dfs(image, sr, sc, color, originalColor);
+        return image;
     }
 
-    public void dfs(int[][] mat, int sr, int sc, int color, int original, boolean[][] visited) {
+    public void dfs(int[][] mat, int sr, int sc, int color, int original) {
     if (sr < 0 || sr >= mat.length || sc < 0 || sc >= mat[0].length) {
         return;
     }
-    
-    if (visited[sr][sc] || mat[sr][sc] != original) {
+    if (mat[sr][sc] != original) {
         return;
     }
-    
-    visited[sr][sc] = true;
     mat[sr][sc] = color;
     
-    dfs(mat, sr, sc + 1, color, original, visited);
-    dfs(mat, sr, sc - 1, color, original, visited);
-    dfs(mat, sr + 1, sc, color, original, visited);
-    dfs(mat, sr - 1, sc, color, original, visited);
+    dfs(mat, sr, sc + 1, color, original);
+    dfs(mat, sr, sc - 1, color, original);
+    dfs(mat, sr + 1, sc, color, original);
+    dfs(mat, sr - 1, sc, color, original);
 }
 }
