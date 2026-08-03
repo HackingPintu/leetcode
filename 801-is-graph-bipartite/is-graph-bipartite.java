849fa93a25788c4c563dbe644dbc88d9b29697cc
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-08-03 11:40:26 +0530

    Time: 1 ms (61.30%) | Memory: 47.4 MB (19.94%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -4,24 +4,43 @@ class Solution {
         Arrays.fill(arr, -1);
         for (int k = 0; k < graph.length; k++) {
             if (arr[k] == -1) {
-                Queue<Integer> queue = new LinkedList<>();
-                queue.add(k);
-                arr[k] = 0;
-                while (!queue.isEmpty()) {
-                    int node = queue.poll();
-                    for (int i = 0; i < graph[node].length; i++) {
-                        int neighbor = graph[node][i];
-                        if (arr[neighbor] == -1) {
-                            arr[neighbor] = 1 - arr[node]; 
-                            queue.add(neighbor);
-                        } else if (arr[neighbor] == arr[node]) {
-                            return false; 
-                        }
-                    }
+                if(!dfs(graph, arr, k)){
+                    return false;
                 }
+                // Queue<Integer> queue = new LinkedList<>();
+                // queue.add(k);
+                // arr[k] = 0;
+                // while (!queue.isEmpty()) {
+                //     int node = queue.poll();
+                //     for (int i = 0; i < graph[node].length; i++) {
+                //         int neighbor = graph[node][i];
+                //         if (arr[neighbor] == -1) {
+                //             arr[neighbor] = 1 - arr[node]; 
+                //             queue.add(neighbor);
+                //         } else if (arr[neighbor] == arr[node]) {
+                //             return false; 
+                //         }
+                //     }
+                // }
             }
         }
         
         return true;
     }
+
+    public boolean dfs(int[][] graph, int[] arr, int node) {
+    for (int i = 0; i < graph[node].length; i++) {
+        int neighbour = graph[node][i];
+        if (arr[neighbour] == -1) {
+            arr[neighbour] = 1 - arr[node];
+            if (!dfs(graph, arr, neighbour)) {
+                return false;
+            }
+        } 
+        else if (arr[neighbour] == arr[node]) {
+            return false;
+        }
+    }    
+    return true;
+}
 }
