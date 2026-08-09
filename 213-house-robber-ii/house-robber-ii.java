dbf9467de5223b64e5a04dfd1f784f60db87a08c
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-08-09 20:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (23.58%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -8,20 +8,24 @@ class Solution {
         // Arrays.fill(dp1, -1);
         // Arrays.fill(dp2, -1);
         // return Math.max(solve1(nums, n-2, dp1), solve2(nums, n - 1, dp2));
-        int[] dp1 = new int[n];
-        dp1[0] = nums[0];
-        dp1[1] = Math.max(nums[0], nums[1]);
+        // int[] dp1 = new int[n];
+        int prev12 = nums[0];
+        int prev11 = Math.max(nums[0], nums[1]);
         for(int i = 2; i < n - 1; i++){
-            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + nums[i]);
+            int curr = Math.max(prev11, prev12 + nums[i]);
+            prev12 = prev11;
+            prev11 = curr;
         }
 
-        int[] dp2 = new int[n];
-        dp2[1] = nums[1];
-        dp2[2] = Math.max(nums[1], nums[2]);
+        // int[] dp2 = new int[n];
+        int prev22 = nums[1];
+        int prev21 = Math.max(nums[1], nums[2]);
         for(int i = 3; i < n; i++){
-            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + nums[i]);
+            int curr = Math.max(prev21, prev22 + nums[i]);
+            prev22 = prev21;
+            prev21 = curr;
         }
-        return Math.max(dp2[n - 1], dp1[n - 2]);
+        return Math.max(prev11, prev21);
     }
 
     // public int solve1(int[] nums, int n, int[] dp){
