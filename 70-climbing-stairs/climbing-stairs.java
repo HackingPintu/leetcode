774ec085e605ba4b905a269040bbb075eb2b8846
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-08-09 19:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.60%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,21 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        
-        if (n <= 1) {
-            return 1;
-        }
-        
-        int prev2 = 1; 
-        
-        int prev1 = 1; 
-        
-        for (int i = 2; i <= n; i++) {
-            int current = prev1 + prev2;
-            
-            prev2 = prev1;
-            prev1 = current;
-        }
-        
-        return prev1;
+        int[] dp = new int[n + 1];
+        return solve(n, dp);
+   
+    }
+
+    public int solve(int n, int[] dp){
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+        if(dp[n] != 0) return dp[n];
+        return dp[n] = solve(n -1, dp) + solve(n - 2, dp);
     }
 }
