a1a784dc52874aa5528ddd733289b8173be25d13
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-08-09 20:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (75.89%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
+        if(n == 1) return nums[0];
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return solve(nums, n - 1, dp);
+        // Arrays.fill(dp, -1);
+        // return solve(nums, n - 1, dp);
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++){
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
+
+        
     }
     
-    public int solve(int[] nums, int n, int[] dp) {
-        if (n < 0) {
-            return 0;
-        }
-        if(dp[n] != -1) return dp[n];
-        int robCurrent = nums[n] + solve(nums, n - 2, dp);
+    // public int solve(int[] nums, int n, int[] dp) {
+    //     if (n < 0) {
+    //         return 0;
+    //     }
+    //     if(dp[n] != -1) return dp[n];
+    //     int robCurrent = nums[n] + solve(nums, n - 2, dp);
         
-        int skipCurrent = solve(nums, n - 1, dp);
+    //     int skipCurrent = solve(nums, n - 1, dp);
         
-        return dp[n] = Math.max(robCurrent, skipCurrent);
-    }
+    //     return dp[n] = Math.max(robCurrent, skipCurrent);
+    // }
 }
