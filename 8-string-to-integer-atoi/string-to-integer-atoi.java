42512a6665b218c9aa0026bfc29bd70a4aa55e91
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-08-12 23:22:03 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (88.54%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,46 +1,34 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        
+        if(s.length() == 0 || s == null) return 0;
         final int INT_MAX = Integer.MAX_VALUE;
         final int INT_MIN = Integer.MIN_VALUE;
-        
-        int i = 0;
-        int n = s.length();
-        
-        while (i < n && s.charAt(i) == ' ') {
-            i++;
-        }
-        
-        if (i == n) {
-            return 0;
-        }
-        
         int sign = 1;
-        if (s.charAt(i) == '+') {
+        int n = s.length();
+        int i = 0;
+        while(i < n && s.charAt(i) == ' '){
             i++;
-        } else if (s.charAt(i) == '-') {
+        }
+
+        if(i == n) return 0;
+
+        if(s.charAt(i) == '-'){
             sign = -1;
             i++;
-        }
-        
-        long res = 0;
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
-            res = res * 10 + digit;
-            
-            if (sign * res <= INT_MIN) {
-                return INT_MIN;
-            }
-            if (sign * res >= INT_MAX) {
-                return INT_MAX;
-            }
-            
+        }else if(s.charAt(i) == '+'){
             i++;
         }
-        
-        return (int)(res * sign);        
+        long res = 0;
+        while(i < n && Character.isDigit(s.charAt(i))){
+            int num = s.charAt(i) - '0';
+            res = res * 10 +  num;
+            if(res * sign <= INT_MIN){
+                return INT_MIN;
+            }
+            if(res * sign >= INT_MAX) return INT_MAX;
+            i++;
+        }
+
+        return (int)(res * sign); 
     }
 }
