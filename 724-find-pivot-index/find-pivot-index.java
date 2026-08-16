2e0b9630becc752c54c9433ca2a691be9f2ca291
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-08-16 20:50:07 +0530

    Time: 1 ms (97.57%) | Memory: 47.2 MB (78.82%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,18 +1,31 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
-        int[] prefixSum = new int[n + 1];
-        for(int i = 1; i <= n; i++){
-            prefixSum[i] = nums[i - 1] + prefixSum[i - 1]; 
-        }
+        // int[] prefixSum = new int[n + 1];
+        int leftSum = 0;
 
-        int pivot = -1;
-        for(int i = 0; i < n; i++){
-            int leftSum = prefixSum[i];
-            int rightSum = prefixSum[n] - prefixSum[i + 1];
+        // for (int i = 1; i <= n; i++){
+        //     prefixSum[i] = nums[i - 1] + prefixSum[i - 1]; 
+        // }
+        int rightSum = 0;
+        for (int i = 1; i < n; i++){
+           rightSum += nums[i];
+        }
+        int i = 0;
+        for (; i < n - 1; i++){
+            // int leftSum = prefixSum[i];
+            // int rightSum = prefixSum[n] - prefixSum[i + 1];
+            // if (leftSum == rightSum) return i;
             if(leftSum == rightSum) return i;
+            leftSum += nums[i];
+            rightSum -= nums[i + 1];
         }
-
-        return pivot;
+        return leftSum == rightSum ? i : -1;
+        // if(i == n - 1){
+        //     leftSum += nums[i];
+        //     rightSum = 0;
+        //     System.out.println(leftSum +" and "+ rightSum);
+        //     if(leftSum == rightSum) return i;
+        // }
     }
 }
