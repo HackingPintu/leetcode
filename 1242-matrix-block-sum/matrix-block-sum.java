c79cc1bbe25d92901060018e240e9ce7a8d174b1
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-08-18 00:18:53 +0530

    Time: 7 ms (63.09%) | Memory: 46.9 MB (8.36%) - LeetSync

---
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] prefix = new int[m + 1][n + 1];
+        for (int i = 1; i < m + 1; i++){
+            for (int j = 1; j < n + 1; j++){
+                prefix[i][j] = mat[i - 1][j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
+            }
+        }
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                int r1 = Math.max(0, i - k);
+                int c1 = Math.max(0, j - k);
+                int r2 = Math.min(m - 1, i + k);
+                int c2 = Math.min(n - 1, j + k);
+
+                r1++;
+                c1++;
+                r2++;
+                c2++;
+
+                ans[i][j] = prefix[r2][c2] - prefix[r1 - 1][c2] - prefix[r2][c1 - 1] + prefix[r1 - 1][c1 - 1];
+            }
+        }
+        // for (int i = 0; i < m; i++){
+        //     for (int j = 0; j < n; j++){
+        //         int sum = 0;
+        //         for (int row = i - k; row <= i + k; row++){
+        //             for (int col = j -k; col <= j + k; col++){
+        //                 if (row >= 0 && row < m && col >= 0 && col < n){
+        //                     sum += mat[row][col];
+        //                 }
+        //             }
+        //         }
+        //         ans[i][j] = sum;
+        //     }
+        // }
+
+        return ans;
+    }
+}
