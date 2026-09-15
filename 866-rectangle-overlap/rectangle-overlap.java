8d608c7cdeb55c15928386e84fd2804afacf1bed
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-09-15 12:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (53.10%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        boolean xAxisCheck = rec1[0] < rec2[2] && rec2[0] < rec1[2] ? true : false;
+        boolean yAxisCheck = rec1[1] < rec2[3] && rec2[1] < rec1[3] ? true : false;
+
+        return xAxisCheck && yAxisCheck;
+
+    }
+}
