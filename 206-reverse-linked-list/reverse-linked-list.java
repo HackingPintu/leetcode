5fc52fb5998d0490d720826d4605007ea035c08e
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-09-16 22:07:30 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (92.22%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,18 +10,32 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode temp = head;
-        ListNode prev = null;
-        while (temp != null){
-            ListNode nextNode = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = nextNode; 
+        // if (head == null || head.next == null) return head;
+        // ListNode temp = head;
+        // ListNode prev = null;
+        // while (temp != null){
+        //     ListNode nextNode = temp.next;
+        //     temp.next = prev;
+        //     prev = temp;
+        //     temp = nextNode; 
 
-        }
+        // }
 
-        return prev;
+        // return prev;
+        return recursion(head);
+    }
+
+    public ListNode recursion(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode newHead = recursion(head.next);
+
+        head.next.next = head;
+
+        head.next = null;
+
+        return newHead;
+        
     }
 
    
