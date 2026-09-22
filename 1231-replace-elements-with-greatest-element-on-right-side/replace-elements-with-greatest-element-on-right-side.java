762c70f92db6606c524700d06c40edbda2818313
Rakesh Roshan Rath <94927905+HackingPintu@users.noreply.github.com>
2026-09-22 15:12:43 +0530

    Time: 2 ms (99.65%) | Memory: 49.8 MB (6.86%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        int max = -1;
+        ans[n - 1] = -1;
+        for (int i = n - 2; i >= 0; i--){
+            if (arr[i + 1] > max){
+                max = arr[i + 1];
+            }
+
+            ans[i] = max;
+
+        }
+
+        return ans;
+    }
+}
